e9a3b47e692da3c6ca0ca0376afe3138e594e7e7
swellweb <info@swellweb.it>
2026-07-25 19:33:38 +0200

    Mark M4 test build as prerelease

--- TargetBridge-Sender/TBDisplaySender/TBDisplaySenderBuildInfo.swift
+++ TargetBridge-Sender/TBDisplaySender/TBDisplaySenderBuildInfo.swift
@@ -1,5 +1,5 @@
 enum TBDisplaySenderBuildInfo {
-    static let marketingVersion = "3.2.0"
+    static let marketingVersion = "3.3.0-m4-5k60-test.1"
     static let buildNumber = "20260711025428"
     static let versionDisplay = "\(marketingVersion) + build \(buildNumber)"
 }
